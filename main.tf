edf74e8c1264ff44aa2340d1f15c4ae444e4e045
anakin1359 <anakin1359@gmail.com>
2022-01-22 22:30:59 +0900

    不要なコメントの削除

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@
 # Terraform Configure
 # ---------------------------
 terraform {
-  required_version = ">=0.13" # 0.13以上のバージョンを指定
+  required_version = ">=0.13"
   required_providers {
     aws = {
-      source  = "hashicorp/aws" # モジュール名を指定
-      version = "~>3.0"         # 3.0以上（マイナーバージョンは無視する）
+      source  = "hashicorp/aws"
+      version = "~>3.0"
     }
   }
 }
