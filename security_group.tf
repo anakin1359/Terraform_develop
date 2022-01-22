9e5221c458e05ecd2048b4bfabbad02ec6152ced
anakin1359 <anakin1359@gmail.com>
2022-01-23 03:53:10 +0900

    application_serverのセキュリティグループ設定

--- security_group.tf
+++ security_group.tf
@@ -4,9 +4,9 @@
 
 # web security group
 resource "aws_security_group" "web_sg" {
-  name        = "${var.project}-${var.environment}-web-sg" # [任意] セキュリティグループの名前（未指定の場合ランダムな文字列が割り当てられる）
-  description = "web front role security group"            # [任意] セキュリティグループの説明（未指定の場合ランダムな文字列が割り当てられる）
-  vpc_id      = aws_vpc.vpc.id                             # [必須] このセキュリティグループが利用出来るVPCを指定
+  name        = "${var.project}-${var.environment}-web-sg"
+  description = "web front role security group"
+  vpc_id      = aws_vpc.vpc.id
 
   tags = {
     Name    = "${var.project}-${var.environment}-web-sg"
@@ -16,12 +16,12 @@ resource "aws_security_group" "web_sg" {
 }
 
 resource "aws_security_group_rule" "web_in_http" {
-  security_group_id = aws_security_group.web_sg.id # [必須] 上記で作成したセキュリティグループのIDを指定
-  type              = "ingress"                    # [必須] ingress=インバウンド通信、egress=アウトバウンド通信
-  protocol          = "tcp"                        # [必須] tcp, udp, icmpなどを指定
-  from_port         = 80                           # [必須] 開始時のポート番号
-  to_port           = 80                           # [必須] 終了時のポート番号
-  cidr_blocks       = ["0.0.0.0/0"]                # [任意] CIDRを指定
+  security_group_id = aws_security_group.web_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "web_in_https" {
@@ -39,7 +39,7 @@ resource "aws_security_group_rule" "web_out_tcp3000" {
   protocol                 = "tcp"
   from_port                = 3000
   to_port                  = 3000
-  source_security_group_id = aws_security_group.app_sg.id # [任意] アクセスを許可したいセキュリティグループのIDを指定
+  source_security_group_id = aws_security_group.app_sg.id
 }
 
 # app security group
@@ -55,7 +55,42 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
-# [補足]: app serverのみルール定義は保留
+# 保留にしていたapp serverのセキュリティグループルールを定義
+resource "aws_security_group_rule" "app_in_tcp3000" {
+  security_group_id        = aws_security_group.app_sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 3000
+  to_port                  = 3000
+  source_security_group_id = aws_security_group.web_sg.id
+}
+
+resource "aws_security_group_rule" "web_out_http" {
+  security_group_id = aws_security_group.app_sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  prefix_list_ids   = [data.aws_prefix_list.s3_pl.id]        # data.tfで定義したS3のプレフィックスリストを取得する ※動的に変わるため「prefix_list_ids」で定義
+}
+
+resource "aws_security_group_rule" "web_out_https" {
+  security_group_id = aws_security_group.app_sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  prefix_list_ids   = [data.aws_prefix_list.s3_pl.id]
+}
+
+resource "aws_security_group_rule" "web_out_tcp3306" {
+  security_group_id        = aws_security_group.app_sg.id
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 3306
+  to_port                  = 3306
+  source_security_group_id = aws_security_group.db_sg.id
+}
 
 # opmng security group
 resource "aws_security_group" "opmng_sg" {
