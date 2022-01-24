730694da3c43ee1058682e0a10652f3e6017fb8c
anakin1359 <anakin1359@gmail.com>
2022-01-25 03:26:34 +0900

    RDS削除前設定（削除防止設定をfalse）

--- rds.tf
+++ rds.tf
@@ -55,23 +55,23 @@ resource "aws_db_instance" "mysql_standalone" {
   # 1. 基本設定
   engine         = "mysql"
   engine_version = "8.0"
-  identifier     = "${var.project}-${var.environment}-mysql-standalone"                # RDSを識別する一意の名前となる
+  identifier     = "${var.project}-${var.environment}-mysql-standalone"
   username       = "admin"
-  password       = random_string.db_password.result                                    # 生成したランダム文字列を使用(属性名resultについて？)
+  password       = random_string.db_password.result
   instance_class = "db.t2.micro"
 
   # 2. ストレージ設定
-  allocated_storage     = 20                                                           # 初期のストレージ容量を指定（単位:ギガバイト）
-  max_allocated_storage = 50                                                           # オートスケールで最大拡張容量を指定（単位: ギガバイト）
-  storage_type          = "gp2"                                                        # default => gp2
-  storage_encrypted     = false                                                        # ストレージの暗号化は行わない（処理が重くなるため）
+  allocated_storage     = 20
+  max_allocated_storage = 50
+  storage_type          = "gp2"
+  storage_encrypted     = false
 
   # 3. ネットワーク設定
-  multi_az               = false                                                       # マルチAZ構成にはしない
-  availability_zone      = "ap-northeast-1a"                                           # シングルAZ構成のためアベイラビリティゾーンを指定
-  db_subnet_group_name   = aws_db_subnet_group.mysql_standalone_subnetgroup.name       # このDBが所属するサブネットグループを指定
-  vpc_security_group_ids = [aws_security_group.db_sg.id]                               # このDBにアタッチするVPCセキュリティグループを指定
-  publicly_accessible    = false                                                       # パブリックでのアクセスは許可しない
+  multi_az               = false
+  availability_zone      = "ap-northeast-1a"
+  db_subnet_group_name   = aws_db_subnet_group.mysql_standalone_subnetgroup.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  publicly_accessible    = false
   port                   = 3306
 
   # 4. DB設定
@@ -80,15 +80,20 @@ resource "aws_db_instance" "mysql_standalone" {
   option_group_name    = aws_db_option_group.mysql_standalone_optiongroup.name
 
   # 5. バックアップ設定
-  backup_window              = "04:00-05:00"                                           # メンテナンス実行前にバックアップを取得
-  backup_retention_period    = 7                                                       # 何日分バックアップを保管するか（1週間分保管する設定）
-  maintenance_window         = "Mon:05:00-Mon:08:00"                                   # メンテナンスの実行タイミングを指定
-  auto_minor_version_upgrade = false                                                   # マイナーアップデートは許可しない
+  backup_window              = "04:00-05:00"
+  backup_retention_period    = 7
+  maintenance_window         = "Mon:05:00-Mon:08:00"
+  auto_minor_version_upgrade = false
 
-  # 6. 削除防止設定
-  deletion_protection = true                                                           # 自動で削除はさせない
-  skip_final_snapshot = false                                                          # 削除時のスナップショットをスキップしない（スナップショットを取得する）
-  apply_immediately   = true                                                           # 削除実行時に即時反映させる
+  # # 6. 削除防止設定(通常時)
+  # deletion_protection = true                                                           # 削除防止を行う
+  # skip_final_snapshot = false                                                          # 削除時のスナップショットをスキップしない（スナップショットを取得する）
+  # apply_immediately   = true                                                           # 削除実行時に即時反映させる
+
+  # 6. 削除防止設定(削除時)
+  deletion_protection = false                                                            # 削除防止を行わない
+  skip_final_snapshot = true                                                             # 削除時のスナップショットをスキップする（スナップショットを取得しない）
+  apply_immediately   = true                                                             # 削除実行時に即時反映させる
 
   # 7. タグ設定
   tags = {
