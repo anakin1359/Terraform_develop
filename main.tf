dd74c2d3a8b424cb5c289f9f30a4e4ead312b9e6
anakin1359 <anakin1359@gmail.com>
2022-02-16 01:55:17 +0900

    .tfstate S3移管

--- main.tf
+++ main.tf
@@ -9,6 +9,12 @@ terraform {
       version = "~>3.0"
     }
   }
+  backend "s3" {
+    bucket  = "tastylog-tfstat-bucket-anakin1359" # S3バケットに設定した名称を指定
+    key     = "tastylog-dev.tfstate"              # 開発環境用のxxx.tfstate（任意の名称を設定）
+    region  = "ap-northeast-1"                    # S3バケットを作成したリージョンを指定
+    profile = "teraform_user"                     # profileで設定しているユーザを指定(cat ~/.aws/configで確認)
+  }
 }
 
 # ---------------------------
